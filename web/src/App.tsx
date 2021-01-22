1c493d237aa282b6ac099988a055bd9df9ac5ac5
= <ryancat.dev@gmail.com>
2021-01-22 16:10:04 -0500

    set up core web layout with nav bar and routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,21 @@
+import { ChakraProvider, extendTheme, theme } from '@chakra-ui/react';
+import { Provider } from 'use-http';
+import Layout from './components/core/Layout';
+
+const customTheme = extendTheme({
+  colors: {
+    brand: theme.colors.green
+  }
+});
+
 const App: React.FC = () => {
-  return <div>Hello World</div>;
+  return (
+    <Provider url={process.env.REACT_APP_API_URL}>
+      <ChakraProvider theme={customTheme}>
+        <Layout />
+      </ChakraProvider>
+    </Provider>
+  );
 };
 
 export default App;
