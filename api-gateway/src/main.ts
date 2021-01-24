1a0c8cf17a60effce23ea9f980aa45864fc47bb2
= <ryancat.dev@gmail.com>
2021-01-24 02:18:20 -0500

    minor tweaks

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app-module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000'
+  });
   app.useGlobalFilters(new ErrorFilter());
   await app.listen(3000);
 }
