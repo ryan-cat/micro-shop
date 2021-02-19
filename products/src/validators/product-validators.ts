85bc17cab2a68ae633c5f846935622e164807b85
= <ryancat.dev@gmail.com>
2021-02-19 11:37:51 -0500

    added validation for product price to only be 2 decimal places

--- products/src/validators/product-validators.ts
+++ products/src/validators/product-validators.ts
@@ -3,6 +3,6 @@ import * as Joi from 'joi';
 export const CreateProductDtoValidator = Joi.object({
   name: Joi.string().required().trim().min(2).max(70),
   description: Joi.string().required().max(500),
-  price: Joi.number().required().greater(0),
+  price: Joi.number().required().precision(2).greater(0),
   imageUrl: Joi.string().uri().required().label('Image url')
 });
